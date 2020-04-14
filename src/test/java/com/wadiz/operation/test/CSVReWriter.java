d8520ec31a3950fccfd976a717d55d6d08bbc18c
진형은 <hyungeun.jin@wadiz.kr>
2020-04-14 18:51:44 +0900

    Initial commit

---
+++ src/test/java/com/wadiz/operation/test/CSVReWriter.java
@@ -0,0 +1,45 @@
+package com.wadiz.operation.test;
+
+import org.junit.Test;
+
+import java.io.*;
+
+/**
+ * @Class com.wadiz.operation.test.CSVReWriter
+ * @Description
+ * @Author hyungeun.jin
+ * @Since 2020. 4. 14.
+ * @Version 1.0
+ * @COPYRIGHT © WADIZ ALL RIGHTS RESERVED.
+ * ------------------------------------------------------------------------
+ * Modification Information
+ * ------------------------------------------------------------------------
+ * 수정일 || 수정자 || 수정내용
+ * ------------------------------------------------------------------------
+ * 2020. 4. 14. || 진형은 || 최초생성
+ */
+public class CSVReWriter {
+
+  @Test
+  public void 가상계좌관련_CSV_ReWriter() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader("/Users/hyungeun.jin/virtualAccount.csv"));
+
+    StringBuilder result = new StringBuilder();
+    long index = 100001;
+    String line = null;
+    while( (line = reader.readLine()) != null){
+      String[] values = line.split("[,]");
+      result
+              .append("088").append(",")
+              .append(values[1].trim()).append(",")
+              .append("186712")
+              .append(System.lineSeparator());
+      index++;
+    }
+    System.out.println(index);
+    System.out.println(result.toString());
+    BufferedWriter writer = new BufferedWriter((new FileWriter("/Users/hyungeun.jin/virtualAccountBucketList.csv")));
+    writer.write(result.toString());
+    writer.flush();
+  }
+}
