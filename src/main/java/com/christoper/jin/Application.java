d7a27d8800dde201ac9cb43629a62bd9ae2afbaf
진형은 <hyungeun.jin@wadiz.kr>
2020-04-14 20:23:08 +0900

    초기 Application 추가

---
+++ src/main/java/com/christoper/jin/Application.java
@@ -0,0 +1,26 @@
+package com.christoper.jin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Class Application
+ * @Description
+ * @Author hyungeun.jin
+ * @Since 2020. 4. 14.
+ * @Version 1.0
+ * @COPYRIGHT © WADIZ ALL RIGHTS RESERVED.
+ * ------------------------------------------------------------------------
+ * Modification Information
+ * ------------------------------------------------------------------------
+ * 수정일 || 수정자 || 수정내용
+ * ------------------------------------------------------------------------
+ * 2020. 4. 14. || 진형은 || 최초생성
+ */
+@SpringBootApplication
+public class Application {
+  public static void main(String[] args){
+    SpringApplication.run(Application.class, args);
+  }
+}
